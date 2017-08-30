e47b83a004aac2153ac9c5a8e26b325b9afab59e
Yihui Xiong <yihui.xiong@hotmail.com>
2017-08-30 16:27:08 +0800

    check sinks attribution first

--- voice_engine/element.py
+++ voice_engine/element.py
@@ -31,8 +31,9 @@ class Element(object):
     def recursive_start(self):
         def recursive_start_sink(s):
             # start downstream first
-            for sink in s.sinks:
-                recursive_start_sink(sink)
+            if hasattr(s, 'sinks'):
+                for sink in s.sinks:
+                    recursive_start_sink(sink)
 
             s.start()
 
@@ -41,8 +42,9 @@ class Element(object):
     def recursive_stop(self):
         def recursive_stop_sink(s):
             # stop downstream first
-            for sink in s.sinks:
-                recursive_stop_sink(sink)
+            if hasattr(s, 'sinks'):
+                for sink in s.sinks:
+                    recursive_stop_sink(sink)
 
             s.stop()
 
