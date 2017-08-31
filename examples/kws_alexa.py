901b283e1847639871e751561d4e8397b57f1174
Yihui Xiong <yihui.xiong@hotmail.com>
2017-08-31 16:24:56 +0800

    add alexa examples

---
+++ examples/kws_alexa.py
@@ -0,0 +1,44 @@
+"""
+Hands-free Voice Assistant with Snowboy and Alexa Voice Service. The wake-up keyword is "snowboy"
+
+Requirement:
+    pip install avs
+"""
+
+
+import time
+import logging
+from voice_engine.source import Source
+from voice_engine.kws import KWS
+from avs.alexa import Alexa
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    src = Source(rate=16000)
+    kws = KWS()
+    alexa = Alexa()
+
+    src.link(kws)
+    kws.link(alexa)
+
+    def on_detected(keyword):
+        logging.info('detected {}'.format(keyword))
+        alexa.listen()
+
+    kws.set_callback(on_detected)
+
+    src.recursive_start()
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    src.recursive_stop()
+
+
+if __name__ == '__main__':
+    main()
---
+++ examples/ns_kws_alexa.py
@@ -0,0 +1,48 @@
+"""
+Hands-free Voice Assistant with Snowboy and Alexa Voice Service. The wake-up keyword is "snowboy"
+
+Requirement:
+    pip install webrtc-audio-processing
+    pip install avs
+"""
+
+
+import time
+import logging
+from voice_engine.source import Source
+from voice_engine.kws import KWS
+from voice_engine.ns import NS
+from avs.alexa import Alexa
+
+
+def main():
+    logging.basicConfig(level=logging.DEBUG)
+
+    src = Source(rate=16000)
+    ns = NS(rate=16000, channels=1)
+    kws = KWS()
+    alexa = Alexa()
+
+    src.link(ns)
+    ns.link(kws)
+    kws.link(alexa)
+
+    def on_detected(keyword):
+        logging.info('detected {}'.format(keyword))
+        alexa.listen()
+
+    kws.set_callback(on_detected)
+
+    src.recursive_start()
+
+    while True:
+        try:
+            time.sleep(1)
+        except KeyboardInterrupt:
+            break
+
+    src.recursive_stop()
+
+
+if __name__ == '__main__':
+    main()
