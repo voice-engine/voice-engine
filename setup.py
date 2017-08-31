3d52f6fe0541415b40688752dc26dbacf4186c85
Yihui Xiong <yihui.xiong@hotmail.com>
2017-08-31 19:13:29 +0800

    0.0.2

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ test_requirements = [
 
 setup(
     name='voice-engine',
-    version='0.0.1',
+    version='0.0.2',
     description="Voice engine to build voice interaction applications",
     long_description=README,
     author="Yihui Xiong",
