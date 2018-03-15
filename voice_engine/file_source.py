0aaac9585568bec8bdf3a42df602264e6fc873ff
Yihui Xiong <yihui.xiong@hotmail.com>
2018-03-15 10:20:44 +0800

    add raw file source

--- voice_engine/file_source.py
+++ voice_engine/file_source.py
@@ -11,18 +11,18 @@ logger = logging.getLogger(__file__)
 
 
 class Source(Element):
-    def __init__(self, wav, frames_size=160):
+    def __init__(self, wav, frame_size=160):
         super(Source, self).__init__()
 
         self._wav = wave.open(wav, 'r')
         self.channels = self._wav.getnchannels()
         self.rate = self._wav.getframerate()
-        self.frames_size = frames_size
+        self.frame_size = frame_size
         self.done = False
 
     def run(self):
         while not self.done:
-            frames = self._wav.readframes(self.frames_size)
+            frames = self._wav.readframes(self.frame_size)
             if not frames:
                 # self._wav.rewind()
                 # continue
---
+++ voice_engine/raw_file_source.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+
+import logging
+import threading
+
+from .element import Element
+
+logger = logging.getLogger(__file__)
+
+
+class Source(Element):
+    def __init__(self, name, rate=16000, channels=1, frame_size=160):
+        super(Source, self).__init__()
+
+        self.name = name
+        self.rate = rate
+        self.channels = channels
+        self.frame_size = frame_size
+        self.done = False
+
+    def run(self):
+        with open(self.name, 'rb') as f:
+            size = self.channels * self.frame_size * 2
+            while not self.done:
+                frames = f.read(size)
+                if not frames:
+                    self.done = True
+                    break
+                elif len(frames) != size:
+                    frames = frames.rjust(size)
+
+                super(Source, self).put(frames)
+
+    def start(self):
+        self.done = False
+
+        thread = threading.Thread(target=self.run)
+        thread.daemon = True
+        thread.start()
+
+    def stop(self):
+        self.done = True
+
+    def is_active(self):
+        return not self.done
