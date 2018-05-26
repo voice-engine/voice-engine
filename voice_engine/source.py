09ff33b9c66846effffc116e0f080107cc745891
Yihui Xiong <yihui.xiong@hotmail.com>
2018-05-26 17:08:01 +0800

    use relative import

--- voice_engine/source.py
+++ voice_engine/source.py
@@ -4,9 +4,9 @@ import os
 
 
 if os.system('which arecord >/dev/null') != 0:
-    from pyaudio_source import Source
+    from .pyaudio_source import Source
 else:
-    from alsa_source import Source
+    from .alsa_source import Source
 
 
 __all__ = ['Source']
