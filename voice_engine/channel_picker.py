c1b2d2fe316ae74e3e70015e37fbf8a4e71ac7cd
Yihui Xiong <yihui.xiong@hotmail.com>
2019-05-16 03:23:08 +0000

    support 32 bits sample width

--- voice_engine/channel_picker.py
+++ voice_engine/channel_picker.py
@@ -10,12 +10,18 @@ from .element import Element
 
 
 class ChannelPicker(Element):
-    def __init__(self, channels=8, pick=0):
+    def __init__(self, channels=8, pick=0, bits_per_sample=16):
         super(ChannelPicker, self).__init__()
         self.channels = channels
         self.pick = pick
+        if bits_per_sample == 16:
+            self.dtype = np.int16
+        elif bits_per_sample == 32:
+            self.dtype = np.int32
+        else:
+            raise ValueError
 
     def put(self, data):
-        data = np.fromstring(data, dtype='int16')
+        data = np.fromstring(data, dtype=self.dtype)
         data = data[self.pick::self.channels].tostring()
         super(ChannelPicker, self).put(data)
